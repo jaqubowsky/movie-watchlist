c10426514f6d54c084630ba28534d5051502f0ab
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-02-13 12:02:19 +0100

    Add remove and add to watchlist button functionality

--- movie-watchlist/events.js
+++ movie-watchlist/events.js
@@ -1,14 +1,69 @@
-import { resetSearchInput, renderFilms } from "./search";
+import { resetSearchInput, renderFilmData, getFilmsArray } from "./search";
+
+import {
+  getLocalStorageArray,
+  setLocalStorageItem,
+  removeLocalStorageItem,
+} from "./watchlist";
 
 const searchForm = document.getElementById("searchForm");
+const filmGrid = document.getElementById("filmGrid");
+const watchlistBtn = document.getElementById("watchlistBtn");
+let SEARCH_INPUT_BEFORE;
 
 const events = (function () {
   searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    const searchInput = document.getElementById("filmSearchInput");
+    SEARCH_INPUT_BEFORE = searchInput.value;
 
-    renderFilms();
+    const filmArray = getFilmsArray(searchInput.value);
+    renderFilmData(filmArray);
+
     resetSearchInput();
   });
-})();
 
+  filmGrid.addEventListener("click", (e) => {
+    const target =
+      e.target.parentElement.parentElement.parentElement.querySelector(
+        ".film-element__title"
+      ).textContent;
+
+    if (
+      e.target.id === "filmAddBtn" ||
+      e.target.classList.contains("btn-icon-add")
+    ) {
+      setLocalStorageItem(target);
+
+      const filmArray = getFilmsArray(SEARCH_INPUT_BEFORE);
+
+      renderFilmData(filmArray);
+    }
+
+    if (
+      e.target.id === "filmRemoveBtn" ||
+      e.target.classList.contains("btn-icon-remove")
+    ) {
+      let filmArray;
+
+      removeLocalStorageItem(target);
+
+      if (!SEARCH_INPUT_BEFORE) {
+        filmArray = getLocalStorageArray();
+      } else {
+        filmArray = getFilmsArray(SEARCH_INPUT_BEFORE);
+      }
+
+      renderFilmData(filmArray);
+    }
+  });
+
+  watchlistBtn.addEventListener("click", () => {
+    SEARCH_INPUT_BEFORE = "";
+
+    const filmArray = getLocalStorageArray();
+
+    renderFilmData(filmArray);
+  });
+})();
 export default events;
